a97e2129632b7fd4319c9e1fc0cd56916ae82add
jmpeUGH <jmpe65@gmail.com>
2023-07-17 14:16:03 +0200

    pequeños detalless antes de crear bbdd

--- src/Controller/AsterixController.php
+++ src/Controller/AsterixController.php
@@ -3,12 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
 class AsterixController extends AbstractController {
 
-    #[Route('/personajes')]
+    #[Route('/personaje', name:'personaje')]
     public function getPersonaje(){
         $personaje=[
             'name'=>'Asterix',
@@ -16,6 +17,40 @@ class AsterixController extends AbstractController {
             'image'=>'https://focus.telerama.fr/627x418/100/2023/01/31/644d4fe1083782d6ce71c0543bf792179f599a74_5022C8134F7BD47B414FEC449F7C4CD0.jpg'
         ];
         
-        return $this->render('Asterix/personajes.html.twig',['personaje'=>$personaje]);
+        return $this->render('Asterix/personaje.html.twig',['personaje'=>$personaje]);
+    }
+
+    #[Route('/listPersonajes', name:'personajes')]
+    public function getPersonajes(){
+        $personajes=[
+        [
+            'name'=>'Asterix',
+            'role'=>'Guerrero galo',
+            'image'=>'https://focus.telerama.fr/627x418/100/2023/01/31/644d4fe1083782d6ce71c0543bf792179f599a74_5022C8134F7BD47B414FEC449F7C4CD0.jpg'
+        ],
+
+        [
+            'name'=>'Asterix',
+            'role'=>'Guerrero galo',
+            'image'=>'https://focus.telerama.fr/627x418/100/2023/01/31/644d4fe1083782d6ce71c0543bf792179f599a74_5022C8134F7BD47B414FEC449F7C4CD0.jpg'
+        ],
+
+        [
+            'name'=>'Asterix',
+            'role'=>'Guerrero galo',
+            'image'=>'https://focus.telerama.fr/627x418/100/2023/01/31/644d4fe1083782d6ce71c0543bf792179f599a74_5022C8134F7BD47B414FEC449F7C4CD0.jpg'
+        ]
+    ];
+        
+        
+        return $this->render('Asterix/listPersonajes.html.twig',['personajes'=>$personajes]);
+    }
+
+
+
+
+    #[Route('/')]
+    public function home(){
+        return new Response("Estoy en la home");
     }
 }
